ee731dacc5763d2de89e1d31085467ac1cc468c9
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-07-11 17:26:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,43 +1,42 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
     domains: [
-      'www.freeimages.com',
-      'cartoonpanti.com',
-      'www.google.com',
-      'aceternity.com'
+      "www.freeimages.com",
+      "cartoonpanti.com",
+      "www.google.com",
+      "aceternity.com"
     ],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'www.freeimages.com',
-        pathname: '/account123/**',
+        protocol: "https",
+        hostname: "www.freeimages.com",
+        pathname: "/account123/**",
       },
       {
-        protocol: 'https',
-        hostname: 'cartoonpanti.com',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "cartoonpanti.com",
+        pathname: "/images/**",
       },
       {
-        
-        protocol: 'https',
-        hostname: 'www.google.com',
-        pathname: '/images/**',
+        protocol: "https",
+        hostname: "www.google.com",
+        pathname: "/images/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.freepik.com',
-        pathname: '/assets/**',
+        protocol: "https",
+        hostname: "www.freepik.com",
+        pathname: "/assets/**",
       },
       {
-        protocol: 'https',
-        hostname: 'aceternity.com',
-        pathname: '/assets/**',
+        protocol: "https",
+        hostname: "aceternity.com",
+        pathname: "/assets/**",
       },
       {
-        protocol: 'https',
-        hostname: 'pexels.com',
-        pathname: '/assets/**',
+        protocol: "https",
+        hostname: "pexels.com",
+        pathname: "/assets/**",
       },
     ],
   },

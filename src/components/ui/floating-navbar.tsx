43c0b51b41e8916454fcb086a0732271b5be7b1c
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-07-11 17:20:15 +0530

    Update floating-navbar.tsx

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -72,7 +72,7 @@ export const FloatingNav = ({
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
-        <button onClick={() => window.location.href = 'https://cartoonpanti.com/'} className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+        <button onClick={() => window.location.href = "https://cartoonpanti.com/"} className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <span>Order</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
         </button>

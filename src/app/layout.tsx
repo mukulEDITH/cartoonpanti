260bc1327da792ed923857b65ddaf613ba0eca8c
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-07-12 10:41:07 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { FloatingNavDemo } from "@/components/FloatingNavDemo";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        
+        <FloatingNavDemo/>
         {children}</body>
     </html>
   );

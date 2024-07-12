201eac5545eb1fdfd0dfc4ba20d8c4ffdf6b6903
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-07-12 15:58:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Homepage from "@/components/Homepage";
 
 export default function Home() {
   return (
-     <div  className="min-h-screen antialiased  ">
+     <div  className="min-h-screen antialiased bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]  " >
       <Homepage/>
      </div>
   );

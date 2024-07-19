9de7da72da559fdec2f1af2ccc3c9a5b574357f7
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-07-19 16:35:51 +0530

    Update Homepage.tsx

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,20 +1,38 @@
 "use client"
-import React from "react"
+
+import React, { useState, useEffect } from 'react';
 import { HeroParallaxDemo , products} from "@/components/HeroParallaxDemo";
 import { InfiniteMovingCardsDemo } from "@/components/InfiniteMovingCardsDemo";
 import { HeroScrollDemo } from "./HeroScrollDemo";
- 
-function Homepage() {
+import { WobbleCardDemo } from "./WobbleCardDemo";
+import LoaderAnimation from '@/components/LoaderAnimation'
+
+const Homepage: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (loading) {
+    return <LoaderAnimation />;
+  }
+
   return (
     <div>
-      
-      <HeroParallaxDemo/>
-      <HeroScrollDemo/>
-      <InfiniteMovingCardsDemo/>
-      <p className="text-center text-sm ">Copyright @mukulEDITH</p>
-      
+      <HeroParallaxDemo />
+      <WobbleCardDemo />
+      <HeroScrollDemo />
+      <InfiniteMovingCardsDemo />
+      <p className="text-center text-sm">Copyright @mukulEDITH</p>
     </div>
-  )
-}
+  );
+};
 
 export default Homepage;
+ 

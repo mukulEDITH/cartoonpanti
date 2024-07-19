b03c6d1f30b0562a86a6f80f648b702d90c6bb82
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-07-19 18:58:35 +0530

    Update LoaderAnimation.jsx

--- src/components/LoaderAnimation.jsx
+++ src/components/LoaderAnimation.jsx
@@ -9,10 +9,14 @@ const LoaderAnimation = () => (
   <div className="flex justify-center items-center  ">
       
     <img src="/assets/c logo.png" alt="Loading animation" className=" flex justify-center items-center  h-100 w-100 bg-gray-200" 
-    style={{ marginTop: '195px' }}
+    style={{ marginTop: '250px' ,
+      transition: 'opacity 0.5s ease',
+      borderRadius: '32px',
+    }}
 
     />
   </div>
 );
 
 export default LoaderAnimation;
+ 

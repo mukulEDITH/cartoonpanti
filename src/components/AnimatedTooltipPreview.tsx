6f88b7d677d3c48363f46b1b42e4d04facf1425d
mukulEDITH <125971038+mukulEDITH@users.noreply.github.com>
2024-07-21 13:31:14 +0530

    Add files via upload

---
+++ src/components/AnimatedTooltipPreview.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
+
+
+
+
+export const people = [
+  {
+    id: 1,
+    name: "Abhijeet Thakur",
+    designation: "CEO & Founder",
+    src:
+      "/assets/chole.png",},
+  {
+    id: 2,
+    name: "Shivam  Nagle",
+    designation: "Graphic designer",
+    src:
+      "/assets/perry.png",
+  },
+  {
+    id: 3,
+    name: "Mukul Kaushal",
+    designation: "Person who support them also a FSD",
+    src:
+      "/assets/ME2.png",
+  },
+  // {
+  //   id: 4,
+  //   name: "Emily Davis",
+  //   designation: "UX Designer",
+  //   src:
+  //     "/assets/f1 f.png",
+  // },
+  
+];
+
+export function AnimatedTooltipPreview() {
+  return (
+    <div className="flex flex-row items-center justify-center   w-full"
+    style={{ marginTop: '150px' ,
+     
+    }}
+    >
+      <AnimatedTooltip items={people} />
+    </div>
+  );
+}
